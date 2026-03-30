885ab36592ebf7849ea2800ba97cadd95253ae39
Villalobos87 <jmvillalobos87@gmail.com>
2026-03-30 10:38:09 -0600

    Aumento

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ st.markdown("---")
 # MÉTRICAS ADICIONALES
 # =========================
 total_cuota_cancelada = df[df["Estado"]=="Pagado"]["Cuota"].sum()
-Capital_Inicial       = 14000
+Capital_Inicial       = 14500
 Ganancias_Entregadas  = 6247.73
 Efectivo              = total_cuota_cancelada + Capital_Inicial - total_prestado - Ganancias_Entregadas
 Pendiente_Recuperar   = df[df["Estado"]=="Pendiente"]["Cuota"].sum()
